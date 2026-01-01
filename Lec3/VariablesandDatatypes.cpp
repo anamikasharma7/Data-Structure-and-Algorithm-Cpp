4f93fada3516b6c4a0bb837c4cdbc889bec53198
Anamika Sharma <140599095+anamikasharma7@users.noreply.github.com>
2026-01-01 11:02:50 +0530

    Learn printing syntax in cpp, Variables and Datatype with memory.

---
+++ Lec3/VariablesandDatatypes.cpp
@@ -0,0 +1,31 @@
+//Variables and Datatypes
+
+#include<iostream>
+using namespace std;
+int main(){
+
+/*Integer:4 bytes
+int a=2, b=4;
+cout<<a+b<<endl;
+*/
+
+/*Character:1 bytes
+char c = 'a';
+cout<<c<<endl;*/
+
+/*Float:4 bytes
+float f = 2.3;
+cout<<f<<endl;*/
+
+/*Double:8 bytes
+double d = 4.76;
+cout<<d<<endl;*/
+
+/*Boolean: 1 bytes
+bool b = 0;
+cout<<b<<endl;*/
+
+
+
+return 0;
+}
---
+++ Lec3/print.cpp
@@ -0,0 +1,9 @@
+//Print Hello World
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello World"<<endl; //Strings
+    cout<<8+3<<endl; //Operations
+    return 0;
+}
